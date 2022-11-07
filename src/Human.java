071fc6a6755394341055ff5a5c5d104b7fc4631c
HeOleg <lu4shemeb@gmao;.com>
2022-11-07 16:28:41 +0300

    Сделал задание 1.

--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Human {
+import java.time.LocalDate;
+
+public class Human {
+
+    String name;
+    int age;
+    String cityOfResidence;
+
+    public Human(String name, int age, String cityOfResidence) {
+        this.name = name;
+        this.age = age;
+        this.cityOfResidence = cityOfResidence;
+    }
+
+    public int getDateOfBirth() {
+        int dateOfBirth = LocalDate.now().getYear() - age;
+        return dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name  +
+               ". Я из города " + cityOfResidence +
+               ". Я родился в " + getDateOfBirth()+ " году. Будем знакомы.";
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Human maksim = new Human("Максим", 35, "Минск");
+        Human anna = new Human("Анна", 29, "Москва");
+        Human katya = new Human("Катя", 28, "Калининград");
+        Human artem = new Human("Артём", 27, "Москва");
+
+        System.out.println(maksim.toString());
+        System.out.println(anna.toString());
+        System.out.println(katya.toString());
+        System.out.println(artem.toString());
+
+
     }
 }
