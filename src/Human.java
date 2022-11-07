9f05de623c70ae679229eff8204fb887253d4d02
HeOleg <lu4shemeb@gmao;.com>
2022-11-07 18:14:09 +0300

    Сделал задание 2.

--- src/Human.java
+++ src/Human.java
@@ -15,14 +15,42 @@ public class Human {
     }
 
     public int getDateOfBirth() {
-        return LocalDate.now().getYear() - age;
+        if (age >= 0) {
+            return LocalDate.now().getYear() - age;
+        } else {
+            return 0;
+        }
+    }
+
+    public String getName() {
+        if (name != null && !name.isEmpty() && !name.isBlank()) {
+            return name;
+        } else {
+            return "<Информация не указана>";
+        }
+    }
+
+    public String getCityOfResidence() {
+        if (cityOfResidence != null && !cityOfResidence.isEmpty() && !cityOfResidence.isBlank()) {
+            return name;
+        } else {
+            return "<Информация не указана>";
+        }
+    }
+
+    public String getPosition() {
+        if (position != null && !position.isEmpty() && !position.isBlank()) {
+            return name;
+        } else {
+            return "<Информация не указана>";
+        }
     }
 
     @Override
     public String toString() {
-        return "Привет! Меня зовут " + name  +
-               ". Я из города " + cityOfResidence +
+        return "Привет! Меня зовут " + getName()  +
+               ". Я из города " + getCityOfResidence() +
                ". Я родился в " + getDateOfBirth()+
-                " году." + "Я работаю на должности " + position + ". Будем знакомы.";
+                " году." + "Я работаю на должности " + getPosition() + ". Будем знакомы.";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Human maksim = new Human("Максим", 35, "Минск","бренд-менеджер");
+        Human maksim = new Human("", 35, "Минск","бренд-менеджер");
         Human anna = new Human("Анна", 29, "Москва", "методист образовательных программ");
         Human katya = new Human("Катя", 28, "Калининград", "продакт-менеджер");
         Human artem = new Human("Артём", 27, "Москва", "директор по развитию бизнеса");
